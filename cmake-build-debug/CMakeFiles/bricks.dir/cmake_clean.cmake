635efc569e708a473fa8ab561f21b123ca7b880c
WIN10 <galsuchetzky@gmail.com>
2019-08-03 00:31:43 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/bricks.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bricks.dir/main.cpp.o"
+  "bricks.pdb"
+  "bricks.exe"
+  "libbricks.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bricks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
